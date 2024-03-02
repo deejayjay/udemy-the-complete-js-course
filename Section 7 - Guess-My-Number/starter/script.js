de04da0820dd57426a662a8ae51bddcf081ab879
deejayjay <deejayjay@zoho.com>
2024-03-02 12:20:50 -0700

    Adds initial files

--- Section 7 - Guess-My-Number/starter/script.js
+++ Section 7 - Guess-My-Number/starter/script.js
@@ -3,5 +3,6 @@
 const message = document.querySelector(".message");
 const number = document.querySelector(".number");
 const score = document.querySelector(".score");
+const guess = document.querySelector(".guess");
 
 message.textContent = "🎉 Correct Number!";
