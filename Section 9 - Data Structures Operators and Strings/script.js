0eb0476c3b8755484c6fb08c57ee3876c19aefb5
deejayjay <deejayjay@zoho.com>
2024-04-02 13:38:47 -0600

    Revises Array Destructuring.

---
+++ Section 9 - Data Structures Operators and Strings/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// Data needed for a later exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+// Array Destructuring
+// -------------------
+const [first, second] = restaurant.categories;
+console.log({ first, second }); // {first: 'Italian', second: 'Pizzeria'}
+
+let [primary, , secondary] = restaurant.categories;
+console.log({ primary, secondary }); // {primary: 'Italian', secondary: 'Vegetarian'}
+
+// Can be used to swap variables
+[secondary, primary] = [primary, secondary];
+console.log({ primary, secondary });
+
+const [starter, main] = restaurant.order(2, 0);
+console.log({ starter, main }); // {starter: 'Garlic Bread', main: 'Pizza'}
+
+// Destructuring nested arrays
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log({ i, j, k }); // {i: 2, j: 5, k: 6}
+
+// Having default values when destructuring
+const [p = 0, q = 0, r = 0] = [8, 9];
+console.log({ p, q, r }); // {p: 8, q: 9, r: 0
